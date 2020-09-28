2810872d36a29248cd5ba0f36817ec5bfd16345a
Cobollero <47508414+Cobollero@users.noreply.github.com>
2020-09-28 16:43:03 +0100

    Add files via upload

--- MESINESP/mer_scripts/gen_mer_lexs.sh
+++ MESINESP/mer_scripts/gen_mer_lexs.sh
@@ -1,15 +1,11 @@
 #!/bin/bash
-#Creates the MER lexicons for usage in the BioASQ competitions.
+#Creates the MER lexicons for usage in MESINESP.
 #Vocabularies:
-# - meshlex: mesh terms and their synonyms
 # - decslex: decs terms and their synonyms
 # - decsparlex: decs terms, their synonyms and direct ancestors
 #
 #HOW TO RUN:
 # ./gen_mer_lexs.sh
-
-echo 'Generating meshlex...'
-python mer_gen_mesh_lex.py
 
 echo 'Generating decslex...'
 python mer_gen_decs_lex.py
